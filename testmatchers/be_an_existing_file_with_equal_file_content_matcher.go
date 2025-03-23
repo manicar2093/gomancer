7dc57f5eeb7fc1d30760e27f9cf21cb806f8152c
Manuel Carbajal <manicar2093@gmail.com>
2025-03-22 23:54:42 -0600

    test: improve matcher to show equal fail output

--- testmatchers/be_an_existing_file_with_equal_file_content_matcher.go
+++ testmatchers/be_an_existing_file_with_equal_file_content_matcher.go
@@ -10,23 +10,32 @@ import (
 
 type BeAnExistingFileWithEqualFileContentMatcher struct {
 	EquallySourceFilePath string
+
+	existingFileMatcher *matchers.BeAnExistingFileMatcher
+	equalMatcher        *matchers.EqualMatcher
+	failExistingMatcher bool
+	failEqualMatcher    bool
+	expectedContent     string
 }
 
 func BeAnExistingFileWithEqualContent(equallySourceFilePath string) types.GomegaMatcher {
 	return &BeAnExistingFileWithEqualFileContentMatcher{
-		equallySourceFilePath,
+		EquallySourceFilePath: equallySourceFilePath,
+		existingFileMatcher:   &matchers.BeAnExistingFileMatcher{},
+		equalMatcher:          &matchers.EqualMatcher{},
 	}
 }
 
-func (c BeAnExistingFileWithEqualFileContentMatcher) Match(actual interface{}) (success bool, err error) {
+func (c *BeAnExistingFileWithEqualFileContentMatcher) Match(actual interface{}) (success bool, err error) {
 	mustExistingFilePath, ok := actual.(string)
 	if !ok {
 		return false, fmt.Errorf("BeAnExistingFileWithEqualFileContentMatcher matcher expects a file path")
 	}
-	existingFileMatcher := matchers.BeAnExistingFileMatcher{}
-	isExisting, err := existingFileMatcher.Match(mustExistingFilePath)
-	if err != nil || !isExisting {
-		return isExisting, err
+
+	success, err = c.existingFileMatcher.Match(mustExistingFilePath)
+	if err != nil || !success {
+		c.failExistingMatcher = true
+		return success, err
 	}
 
 	mustExistingSourceContent, err := os.ReadFile(mustExistingFilePath)
@@ -39,17 +48,33 @@ func (c BeAnExistingFileWithEqualFileContentMatcher) Match(actual interface{}) (
 		return false, err
 	}
 
-	equalMatcher := matchers.EqualMatcher{
-		Expected: mustExistingSourceContent,
+	c.equalMatcher.Expected = string(mustExistingSourceContent)
+	success, err = c.equalMatcher.Match(equallySourceContent)
+	if err != nil || !success {
+		c.failEqualMatcher = true
+		c.expectedContent = string(equallySourceContent)
+		return success, err
 	}
-	return equalMatcher.Match(equallySourceContent)
+	return
 
 }
 
-func (c BeAnExistingFileWithEqualFileContentMatcher) FailureMessage(actual interface{}) (message string) {
+func (c *BeAnExistingFileWithEqualFileContentMatcher) FailureMessage(actual interface{}) (message string) {
+	if c.failExistingMatcher {
+		return c.existingFileMatcher.FailureMessage(actual)
+	}
+	if c.failEqualMatcher {
+		return c.equalMatcher.FailureMessage(c.expectedContent)
+	}
 	return format.Message(actual, "to exist and match content")
 }
 
-func (c BeAnExistingFileWithEqualFileContentMatcher) NegatedFailureMessage(actual interface{}) (message string) {
+func (c *BeAnExistingFileWithEqualFileContentMatcher) NegatedFailureMessage(actual interface{}) (message string) {
+	if c.failExistingMatcher {
+		return c.existingFileMatcher.NegatedFailureMessage(actual)
+	}
+	if c.failEqualMatcher {
+		return c.equalMatcher.NegatedFailureMessage(actual)
+	}
 	return format.Message(actual, "not to exist and not match content (this has no sense any way ._.)")
 }

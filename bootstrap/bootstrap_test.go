a0578930b2cadfddfd7ded2587c9929cb92a5a9e
Manuel Carbajal <manicar2093@gmail.com>
2025-06-06 21:50:17 -0600

    feat: add \t to makefile generation

--- bootstrap/bootstrap_test.go
+++ bootstrap/bootstrap_test.go
@@ -311,15 +311,15 @@ tasks:
 		It("creates Makefile file", func() {
 			content := `# Run templ generation in watch mode
 templ:
-    go tool templ generate --watch --proxy="http://localhost:8090" --open-browser=false -v
+	go tool templ generate --watch --proxy="http://localhost:8090" --open-browser=false -v
 
 # Watch Tailwind CSS changes
 tailwind:
-    tailwindcss -i ./cmd/service/sources/css/input.css -o ./cmd/service/assets/css/styles.css --watch
+	tailwindcss -i ./cmd/service/sources/css/input.css -o ./cmd/service/assets/css/styles.css --watch
 
 # Start development server with all watchers
 dev:
-    go tool air
+	go tool air
 `
 			Expect(dirWithPath("Makefile")).Should(testmatchers.BeAnExistingFileAndEqualString(content))
 		})

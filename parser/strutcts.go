08ea88a407966059cb2522e5f02e983a4bb22b10
Manuel Carbajal <manicar2093@gmail.com>
2025-07-10 19:25:12 -0600

    feat: add way to get string shell cmd

--- parser/strutcts.go
+++ parser/strutcts.go
@@ -1,5 +1,10 @@
 package parser
 
+import (
+	"github.com/manicar2093/gomancer/types"
+	"strings"
+)
+
 type (
 	TransformedText struct {
 		SnakeCase        string
@@ -25,3 +30,31 @@ type (
 		Attributes        []Attribute
 	}
 )
+
+func (c GenerateModelInput) ToStringCmd() string {
+	sb := &strings.Builder{}
+	sb.WriteString("gomancer gen ")
+	if types.SupportedType(c.IdAttribute.Type) == types.TypeUuid {
+		sb.WriteString("--pk-uuid ")
+	}
+	sb.WriteString(c.PascalCase)
+	sb.WriteString(" ")
+	for _, attr := range c.Attributes {
+		sb.WriteString(attr.SnakeCase)
+		sb.WriteString(":")
+		sb.WriteString(attr.Type)
+		if types.SupportedType(attr.Type) == types.TypeEnum {
+			for _, enum := range attr.EnumStrings {
+				sb.WriteString("/")
+				sb.WriteString(enum.SnakeCase)
+			}
+		}
+		if attr.IsOptional {
+			sb.WriteString(":")
+			sb.WriteString("optional")
+		}
+		sb.WriteString(" ")
+	}
+
+	return sb.String()
+}

a071d3313a1120254db4c08887f5bc93409b3dad
Manuel Carbajal <manicar2093@gmail.com>
2026-04-25 00:28:16 -0600

    feat: add more details in generated readme

--- bootstrap/bootstrap_test.go
+++ bootstrap/bootstrap_test.go
@@ -533,24 +533,7 @@ keep_scroll = true
 		})
 
 		It("creates README.md file", func() {
-			content := `# test
-
-This is the initial code for the greatest project ever made.
-
-## Tools
-
-Yep, you need to install some tools to make this work:
-
-- [Go](https://go.dev/): That's why you are here, right? :P
-- [Taskfile](https://taskfile.dev/): A task runner used instead Makefile
-- [Air](https://github.com/air-verse/air): Live reload tool for Go apps
-- [Node](https://nodejs.org/es): Needed to run npx command
-
-## Thank you!
-
-Be yourself. Find joy by what you do. Happy coding :)
-`
-			Expect(dirWithPath("README.md")).Should(testmatchers.BeAnExistingFileAndEqualString(content))
+			Expect(dirWithPath("README.md")).Should(testmatchers.BeAnExistingFileWithEqualContent(path.Join("fixtures", "readme.txt")))
 		})
 
 		It("creates prisma.config.ts file", func() {
